feca99f4736c54dbe5ae8d28fa40391031f1eb1d
Ruichen Teng <tengrc1106@gmail.com>
2017-02-06 01:33:24 +1100

    init user system

---
+++ src/main/java/tengr/controllers/MainController.java
@@ -0,0 +1,17 @@
+package tengr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  @ResponseBody
+  public String index() {
+    return "Proudly handcrafted by " +
+        "<a href='http://netgloo.com/en'>Netgloo</a> :)";
+  }
+
+}
